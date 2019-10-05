d625d23891d57066a872a7752336e7a4936d9997
Artur <aluno@alunovm-sisop.(none)>
2019-10-04 21:50:00 -0300

    reescrevemos a emplace :)

--- exemplos/exemplo.c
+++ exemplos/exemplo.c
@@ -9,7 +9,7 @@
  */
 
 #include "../include/support.h"
-//#include "../include/cthread.h"
+#include "../include/cthread.h"
 #include <stdio.h>
 
 void* func0(void *arg) {
@@ -27,15 +27,13 @@ int main(int argc, char *argv[]) {
 	int	id0, id1;
 	int i = 10;
 
-	printf("abriu");
-
-//	id0 = ccreate(func0, (void *)&i, 0);
-//	id1 = ccreate(func1, (void *)&i, 0);
+	id0 = ccreate(func0, (void *)&i, 0);
+	id1 = ccreate(func1, (void *)&i, 0);
 
 	printf("Eu sou a main após a criação de ID0 e ID1\n");
 
-//	cjoin(id0);
-//	cjoin(id1);
+	cjoin(id0);
+	cjoin(id1);
 
 	printf("Eu sou a main voltando para terminar o programa\n");
 
